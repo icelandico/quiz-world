41d23e6ff61f8d9b8de7401b772e5086000ac53e
Michal Muszynski <m.muszy@gmail.com>
2019-08-07 16:59:01 +0200

    Store config

--- src/store/app-store.ts
+++ src/store/app-store.ts
@@ -5,64 +5,19 @@ const API_COUNTRIES = "http://54.72.28.201:80/1.0/countries"
 const API_POPULATION =
   "http://54.72.28.201:80/1.0/population/Brazil/2015-12-24/"
 
-interface IStore {
-  currentPage: number
-  itemsPerPage: number
-  repositories: any
-  loading: boolean
-  refresh: boolean
-}
+interface IStore {}
 
-export default class Repos implements IStore {
-  currentPage = 1
-
-  itemsPerPage = 10
-
-  repositories: any = []
-
+export default class Questions implements IStore {
   loading = false
-
-  refresh = false
-
-  fetchData(input: string) {
-    this.clearCurrentPage()
-    fetch(`${APIQuery}${input}&per_page=100`)
-      .then(res => res.json())
-      .then(data => (this.repositories = data.items || []))
-      .then(() => (this.loading = false))
-      .catch(e =>
-        alert(
-          `Something went wrong. Please try again or try to understand the error: ${e} .`
-        )
-      )
-  }
-
-  changeCurrentPage(page: number) {
-    this.currentPage = page
-  }
 
   changeLoadingState(state: boolean) {
     this.loading = state
   }
-
-  clearCurrentPage() {
-    this.currentPage = 1
-  }
-
-  clearRepositories() {
-    this.repositories = []
-  }
 }
 
-decorate(Repos, {
-  currentPage: observable,
-  refresh: observable,
-  fetchData: action,
-  repositories: observable,
-  loading: observable,
-  changeLoadingState: action,
-  changeCurrentPage: action
+decorate(Questions, {
+  loading: observable
 })
 
-const ReposStore = new Repos()
-export { ReposStore }
+const QuizStore = new Questions()
+export { QuizStore }

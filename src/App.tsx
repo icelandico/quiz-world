667f78cd501ea50d14879c764c6d405beeb68d8d
Michal Muszynski <m.muszy@gmail.com>
2019-08-07 20:38:38 +0200

    Generate and render questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useEffect, useContext } from "react"
 import Header from "./components/Header/header"
 import Main from "./components/Main/Main"
 import { AppContainer } from "./app-styles"
+import QuizStore from "./store/app-store"
 
 const App: React.FC = () => {
+  const store = useContext(QuizStore)
+
+  useEffect(() => {
+    store.fetchCountries()
+    // store.generateQuestionsSet()
+  }, [])
+
   return (
     <AppContainer>
       <Header />
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,20 +1,29 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { observer } from "mobx-react-lite"
 import ButtonContainer from "./ButtonContainer/button-container"
 import QuizStore from "../../store/app-store"
 
 const Main: React.FC = observer(() => {
   const store = useContext(QuizStore)
+
+  const renderLoading = () => {
+    return <h2>LOADIN</h2>
+  }
   return (
     <div>
       <ButtonContainer />
-      {store.loading ? (
-        <p>"LOADING"</p>
-      ) : (
-        store.countries.map(el => {
-          return <p>{el}</p>
-        })
-      )}
+      <div>
+        <h2>Question:</h2>
+        <div>
+          {store.questions.length === 0 && store.loading
+            ? renderLoading()
+            : store.questions.length > 0
+            ? store.questions.map(el => {
+                return <p>{el.country}</p>
+              })
+            : null}
+        </div>
+      </div>
     </div>
   )
 })

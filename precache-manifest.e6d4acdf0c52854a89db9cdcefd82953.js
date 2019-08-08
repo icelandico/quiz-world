52f2563bfb95cc776968efdbcca63cf107321bbd
Michal Muszynski <m.muszy@gmail.com>
2019-08-08 22:33:07 +0200

    Updates

---
+++ precache-manifest.e6d4acdf0c52854a89db9cdcefd82953.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3bc06e5852089885d6ba486266dd5f",
+    "url": "/quiz-world/index.html"
+  },
+  {
+    "revision": "27f541719bd4e92d4aef",
+    "url": "/quiz-world/static/css/main.61d90e07.chunk.css"
+  },
+  {
+    "revision": "eb3027c777cfc491716c",
+    "url": "/quiz-world/static/js/2.d6ec383b.chunk.js"
+  },
+  {
+    "revision": "27f541719bd4e92d4aef",
+    "url": "/quiz-world/static/js/main.6c0f802b.chunk.js"
+  },
+  {
+    "revision": "76d42d45dab67735f1a4",
+    "url": "/quiz-world/static/js/runtime~main.cbf3b63b.js"
+  }
+]);
